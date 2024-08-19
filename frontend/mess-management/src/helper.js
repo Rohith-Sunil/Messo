95f11a486d911c079032b9249d6eca02f22c75d7
Rohith Sunil <117839479+Rohith-Sunil@users.noreply.github.com>
2024-08-19 18:40:22 +0530

    Update helper.js

--- frontend/mess-management/src/helper.js
+++ frontend/mess-management/src/helper.js
@@ -1,2 +1,3 @@
-export const baseUrl = "https://messo-backend.onrender.com";
+// export const baseUrl = "https://messo-backend.onrender.com";
+export const baseUrl = "https://messo-nine.vercel.app/";
 // export const baseUrl = "http://localhost:5000";
